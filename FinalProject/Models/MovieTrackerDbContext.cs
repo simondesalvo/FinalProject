90e7075407f6446b944c9e583d48cbe9ab769f9b
JordanHallam <jghals12@gmail.com>
2020-09-11 16:48:56 -0400

    Connectingtto azure and adding wireframes

---
+++ FinalProject/Models/MovieTrackerDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace FinalProject.Models
+{
+    public partial class MovieTrackerDbContext : DbContext
+    {
+        public IConfiguration Configuration { get; }
+        public MovieTrackerDbContext()
+        {
+        }
+
+        public MovieTrackerDbContext(DbContextOptions<MovieTrackerDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
