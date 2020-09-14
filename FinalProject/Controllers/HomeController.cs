d350fb5e18fe82f72dda61e91e8666d60b5bc79d
gitUserSM <sumana_manoj@yahoo.com>
2020-09-14 15:14:11 -0400

    Added Create and Update UserMovie info + Genre Info

--- FinalProject/Controllers/HomeController.cs
+++ FinalProject/Controllers/HomeController.cs
@@ -7,18 +7,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FinalProject.Models;
 using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
 
 namespace FinalProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly MovieDAL _movieDAL;
-        private readonly string _apikey;        
+        private readonly string _apikey;
+        string loginUserId;
+        private readonly MovieTrackerDbContext _context;
 
         public HomeController(IConfiguration configuration)
         {
             _apikey = configuration.GetSection("ApiKeys")["MovieAPIKey"];
             _movieDAL = new MovieDAL(_apikey);
+            loginUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
         }
 
         public IActionResult Index()
@@ -34,7 +38,7 @@ namespace FinalProject.Controllers
 
         public async Task<IActionResult> SearchResult(string userInput)
         {
-            List<MovieSearch> movies = await _movieDAL.GetMovies($"{userInput}");
+            List<MovieSearch> movies = await _movieDAL.GetMovies($"{userInput}");            
             return View("SearchResult", movies);
         }
 
@@ -52,6 +56,62 @@ namespace FinalProject.Controllers
             return movie;
         }
 
+        public IActionResult AddtoWatchedList(PopcornMovie popcornMovie)
+        {
+            UserMovie userMovie = new UserMovie();
+            userMovie.UserId = loginUserId;
+            userMovie.MovieId = popcornMovie.imdbID;
+            userMovie.Watched = false;
+            try
+            {
+                _context.UserMovie.Add(userMovie);
+                _context.SaveChanges();
+                AddtoGenre(popcornMovie);
+            }
+            catch
+            {
+
+            }
+            return RedirectToAction("DisplayList");
+        }
+
+        public bool AddtoGenre(PopcornMovie popcornMovie)
+        {            
+            Genre newEntry = new Genre();
+            newEntry.Imdbid = popcornMovie.imdbID;
+            newEntry.Genre1 = popcornMovie.Genre;
+            UniqueGenre uGenre = new UniqueGenre();
+            try
+            {
+                _context.Genre.Add(newEntry);
+                _context.SaveChanges();
+                if(!uGenre.uniqueGenres.Contains(popcornMovie.Genre))
+                {
+                    uGenre.uniqueGenres.Add(popcornMovie.Genre);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public IActionResult UpdateWatchedList(UserMovie userMovie)
+        {
+            UserMovie userMovieToUpdate = _context.UserMovie.Find(userMovie.Id);
+            userMovieToUpdate.Watched = userMovie.Watched;
+            userMovieToUpdate.WatchLocation = userMovie.WatchLocation;
+            userMovieToUpdate.WatchYear = userMovie.WatchYear;
+            userMovieToUpdate.UserRating = userMovie.UserRating;
+            userMovieToUpdate.UserReview = userMovie.UserReview;
+            // TO BE IMPLEMENTED LATER
+            //userMovieToUpdate.WatchedTogetherId = ;     
+            _context.UserMovie.Update(userMovieToUpdate);
+            _context.SaveChanges();
+
+            return RedirectToAction("DisplayList");
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ FinalProject/Models/MovieVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class MovieVM
+    {
+        public MovieSearch movieSearch { get; set; }
+        public PopcornMovie popcornMovie { get; set; }
+        public Genre genre { get; set; }
+        public MovieActor movieActor { get; set; }
+        public Movie movie { get; set; }
+
+    }
+}
