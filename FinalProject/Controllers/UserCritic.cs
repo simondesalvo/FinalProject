70885862688df2018f1c669376c4fdf40cc14f07
simondesalvo <simondesalvo@gmail.com>
2020-09-15 14:36:03 -0400

    added delete functions

---
+++ FinalProject/Controllers/UserCritic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using FinalProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+
+namespace FinalProject.Controllers
+{
+    public class UserCritic : Controller
+    {
+        private readonly MovieDAL _movieDAL;
+        private readonly MovieTrackerDbContext _context;
+        private readonly string _apikey;
+
+        public UserCritic(IConfiguration configuration, MovieTrackerDbContext context)
+        {
+            _apikey = configuration.GetSection("ApiKeys")["MovieAPIKey"];
+            _movieDAL = new MovieDAL(_apikey);
+            _context = context;
+
+        }
+
+        //api call
+        public async Task<IActionResult> MovieSelection(string id)
+        {
+            var selection = await _movieDAL.SecondGetMovieInfo($"{id}");
+            return View(selection);
+
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            List<UserMovie> savedMovies = _context.UserMovie.Where(x => x.UserId == userId).ToList();
+            List<UserMovie> userList = new List<UserMovie>();
+            foreach (UserMovie m in savedMovies)
+            {
+                userList.Add(m);
+            }
+            //list of movies user has saved
+
+
+
+            return View();
+        }
+    }
+}
